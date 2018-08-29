4ff4af1c3e5d2f8e87de24bb8388866612ade544
wanshun123 <citexs@gmail.com>
2018-08-29 12:22:30 +0800

    Update rateme.py

--- rateme.py
+++ rateme.py
@@ -11,13 +11,24 @@ api = PushshiftAPI()
 
 submissions = list(api.search_submissions(subreddit='rateme',
                             filter=['url','id', 'title'],
-                            limit=10))
+                            limit=50))
 submissions = [v[1:4] for v in submissions]
 
 import re
 p = re.compile("[0-9]+\/? ?[MmFf]|[MmFf]\/? ?[0-9]+")
-p.findall(submissions[1][1])[0]
-                            
+#p.findall(submissions[0][1])[0]
+
+#re.search("[0-9]+\/? ?[MmFf]|[MmFf]\/? ?[0-9]+", submissions[3][1])[0]
+
+age = []
+sex = []
+
+for i in submissions:
+    if len(p.findall(i[1])) > 0:
+        print(p.findall(i[1])[0] + ' is the complete match')
+        re.search("[MmFf]", p.findall(i[1])[0])[0]
+        re.search("[0-9]+", p.findall(i[1])[0])[0]
+    
 comments = list(api.search_comments(subreddit='rateme', filter=['body', 'link_id'], limit=10))
 comments = [v[::2] for v in comments]
 comments = [v[0:2] for v in comments]
